1d73c127ef6657f07f71af81216c65c9f977717e
Cailyn Serratt <123019302+cailynserratt@users.noreply.github.com>
2024-09-06 23:46:25 -0500

    Variables

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log("Hey! What is your favorite flower?");
-document.write("Please answer the question in the console using F12. Have a nice day!")
+document.write("Please press F12 and check the console!")
+var name = "Remaining Health Bar" // this is the name displayed - GLOBAL STRING
+
+const character = {model: "Male", HP: "1500", class: "Druid"} // this is the character description - OBJECT
+
+let life = 1500;  // this is the life bar - BLOCK NUMBER
+
+life = life - 275; // character takes one hit, loses health
+life = life - 333; // character takes another hit, loses health
+life = life + 150; // character takes health potion, restores health
+
+console.log(character);
+console.log(name);
+console.log(life);
