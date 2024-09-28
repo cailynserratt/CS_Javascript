7166dade489881a44034d8cf273f5f297bb00629
Cailyn Serratt <123019302+cailynserratt@users.noreply.github.com>
2024-09-28 00:51:51 -0500

    5.4 Assignment

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
-document.write("Please press F12 and check the console for a small game!")
-const HP = 23;
-const healthPotion = 100;
-const potions = "0";
+const characterclasses= ["Elf", "Druid", "Barbarian", "Bard", "Cleric", "Paladin", "Rogue"];
+let health = 0;
 
-console.log(
-    `Hello Player! Your HP is currently at ${HP} and health potions restore ${healthPotion} HP! Have Fun!`
-)
-
-if (HP >= 50) {
-    console.log("Your HP is looking good!");
-} else if (HP < 25) {
-    console.log("Wow your HP is super low...take a health potion to heal...");
-} else {
-    console.log("Your HP could be better.")
+for (characterclass of characterclasses) {
+  console.log(characterclass);
+  if (characterclass === "Rogue") {
+    console.log("Rogue is an available class!");
+    break;
+  }
 }
 
-const combined = HP + healthPotion;
-console.log("You took a health potion! " + combined + " is now your total HP!")
+while(health < 100) {
+  console.log("Your health is still recovering!")
 
-switch (potions) {
-    case "0":
-        console.log("You have no potions left!");
-      break;
-    case "1":
-        console.log("You have 1 potions left!");
-      break;
-    case "2":
-        console.log("You have 2 potions left!");
-      break;
-    case "3":
-        console.log("You have 3 potions left!");
-      break;
-    case "4":
-        console.log("You have more than 3 potions left!");
-      break;
-    default:
-        console.log("This is your potion stock!")
-        break;
-  }
+  health++;
+}
